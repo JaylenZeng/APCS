83d97ae74815ce2872dcb620dd054d07d92c0c1d
Jaylen <jzeng30@stuy.edu>
2021-09-30 22:02:52 -0400

    init setup

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,6 +1,12 @@
 public class Greet {
-	public static void main (String [] args) {
-		System.out.println("No hablo queso");
+	static void greetmethod(String name) {
+		System.out.println("How's it going"+name+"? Good to see you again!");
+	}
+
+	public static void main(String[] args) {
+		greetmethod(" REDACTED");
+		greetmethod(" John Adams");
+		greetmethod(" Billy");
 	}
 }
 
