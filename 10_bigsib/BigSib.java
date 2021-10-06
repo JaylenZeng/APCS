3e58ff83867d1fd67cfcfffd3f4cbbecbd5ff2f0
Jaylen <jzeng30@stuy.edu>
2021-10-06 19:51:49 -0400

    init setup

--- 10_bigsib/BigSib.java
+++
@@ -1,7 +0,0 @@
-public class BigSib {
-        public static String greet(String name) {
-                String s;
-                s = ("How's it going"+name+"? Good to see you again!");
-                return s;
-        }
-}
