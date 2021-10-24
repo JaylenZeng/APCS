7a9716173ecd27e6dedccceeebea6478b061ea89
Jaylen <jzeng30@stuy.edu>
2021-10-23 21:52:42 -0400

    added hw

--- Work/23_Coin/Driver.java
+++ Work/23_Coin/Driver.java
@@ -1,14 +1,3 @@
-/***
-    driver for class Coin
-    ~~~ SUGGESTED WORKFLOW: ~~~
-    1. Compile this file and run. Note anything notable.
-    2. Move the "TOP" line down, so that it is below the first statement.
-    (emacs: with cursor at beginning of TOP line, C-k C-k, DOWN, DOWN, C-y)
-    (your editor: ???)
-    3. Compile and run again.
-    4. Resolve errors one at a time until it works.
-    5. Repeat 2-4 until TOP meets BOTTOM.
-***/
 
 public class Driver {
 
@@ -18,16 +7,15 @@ public class Driver {
 
 
     //test default constructor
-    /*===================TOP==========================
       Coin mine = new Coin();
       //test 1st overloaded constructor
       Coin yours = new Coin( "quarter" );
       //test 2nd overloaded constructor
       Coin wayne = new Coin( "dollar", "heads" );
       //test toString() methods of each Coin
-      System.out.println("mine: " + mine);
-      System.out.println("yours: " + yours);
-      System.out.println("wayne: " + wayne);
+      System.out.println("mine: " + mine.toString());
+      System.out.println("yours: " + yours.toString());
+      System.out.println("wayne: " + wayne.toString());
       //test flip() method
       System.out.println("\nAfter flipping...");
       yours.flip();
@@ -41,6 +29,7 @@ public class Driver {
       else {
         System.out.println( "No match. Firestarter you can not be." );
       }
+    /*===================TOP==========================
       ====================BOTTOM======================*/
 
   }//end main()
