d928f29d9f64446adbf8c53c3d5253703c27ec1c
Jaylen <jzeng30@stuy.edu>
2021-11-22 12:59:14 -0500

    updates

--- Work/yoRPG/Protagonist.java
+++ Work/yoRPG/Protagonist.java
@@ -1,7 +1,8 @@
 public class Protagonist {
-  private String name;
-  private Boolean isAlive = true;
-  private int hp = 100;
+  public String name;
+  public Boolean isAlive = true;
+  public int hp = 100;
+  public int dmg;
 
   public Protagonist(String GivenName) {
     name = GivenName;
@@ -12,8 +13,19 @@ public class Protagonist {
   public Boolean isAlive() {
     return isAlive;
   }
+
+  public void normalize() {
+    dmg = (int) (Math.random() * 20);
+  }
+
+  public void specialize() {
+    dmg = (int) (Math.random() * 20 * 1.5);
+  }
+
   public int attack(Monster target) {
-    
+    target.hp -= dmg;
+    return dmg;
   }
 
 }
+
