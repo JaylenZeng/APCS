f4ca3dfb1c1922837175e074d0cadaf075b730cf
JaylenZeng <jaylenzeng@gmail.com>
2022-03-15 20:48:36 -0400

    fix List.java

--- 77_llist/List.java
+++ 77_llist/List.java
@@ -23,7 +23,7 @@ public interface List
   public boolean add( String x );
 
   //insert at specified index
-  public void add( int index, String newVal )
+  public void add( int index, String newVal );
 
   //remove node from list, return its data
   public String remove( int index );
