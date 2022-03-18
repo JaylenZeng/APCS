3fe30a4150ced2442244b81a833fd9efdfca6521
Jaylen <jzeng30@stuy.edu>
2022-03-18 13:16:54 -0400

    add lab

--- cobo/011_elevens/Activity2  Starter Code/DeckTester.java
+++ cobo/011_elevens/Activity2  Starter Code/DeckTester.java
@@ -8,6 +8,20 @@ public class DeckTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		Deck one = new Deck(
+			new String[]{"Three", "Seven", "Nine", "King"},
+			new String[]{"Hearts", "Diamonds", "Clubs", "Spades"},
+			new int[]{3, 7, 9, 13}		
+		);
+		Deck two = new Deck(
+			new String[]{"One", "Two", "Three", "Four"},
+			new String[]{"Spades", "Spades", "Spades", "Spades"},
+			new int[]{1, 2, 3, 4}
+		);
+		Deck three = new Deck(
+			new String[]{"Five", "Five", "Five", "Five"},
+			new String[]{"Diamonds", "Clubs", "Hearts", "Spades"},
+			new int[]{5, 5, 5, 5}
+		);
 	}
 }
