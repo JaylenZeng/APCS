b1b2f84b5ac80947050d7687dabf9dfe9b621bfd
Jaylen <jzeng30@stuy.edu>
2022-04-26 12:49:42 -0400

    add cobo lab

--- fallsem/24_while/Test.java
+++
@@ -1,9 +0,0 @@
-class Test {
-	Coin drop = new Coin();
-	Coin dead = new Coin();
-	Coin fred = new Coin();
-	System.out.println( drop == dead);
-	System.out.println( drop == fred);
-	System.out.println( drop.equals(dead));
-	System.out.println( drop.equals(fred));
-}
